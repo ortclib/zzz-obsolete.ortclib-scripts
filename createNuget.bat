048552bd7347f42213e1e1050925c3ed8f4d4a16
Sergej Jovanovic <sergej@hookflash.com>
2016-05-29 15:07:50 +0200

    Enabled ortc project build

--- createNuget.bat
+++ createNuget.bat
@@ -117,8 +117,8 @@ if "%failure%" neq "0" goto:endedWithError
 call:determineVisualStudioPath
 if "%failure%" neq "0" goto:eof
 
-::call:build
-::if "%failure%" neq "0" goto:eof
+call:build
+if "%failure%" neq "0" goto:eof
 
 call:preparePackage
 if "%failure%" neq "0" goto:eof
