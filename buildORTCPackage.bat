ca802f0852affb250628ecbf0b8d153f76a01af4
Sergej Jovanovic <sergej@hookflash.com>
2016-06-09 14:01:58 +0200

    Created a batch script that clones the ortc-lib-sdk repository, runs prepare and create nuget scripts.

---
+++ buildORTCPackage.bat
@@ -0,0 +1,60 @@
+@echo off
+
+SET REPOSITORY=https://github.com/openpeer/ortc-lib-sdk.git
+SET BRANCH=%1
+SET failure=0
+SET REPOSITORY_DIR=ortc-lib-sdk
+call:clone
+if "%failure%" neq "0" goto:eof
+
+echo Repository is successfully cloned
+
+call:prepare
+if "%failure%" neq "0" goto:eof
+
+call:createNuget
+if "%failure%" neq "0" goto:eof
+goto:done
+
+:clone
+if "%BRANCH%" neq "" (
+	echo Cloning branch %BRANCH%
+	git clone --recursive %REPOSITORY% -b "%BRANCH%"
+) else (
+	echo Cloning master
+	git clone --recursive %REPOSITORY%
+)
+if ERRORLEVEL 1 call:failure %errorlevel% "Could not make a directory %1"
+goto:eof
+
+:prepare
+pushd %REPOSITORY_DIR%
+call bin\prepare.bat
+if ERRORLEVEL 1 call:failure %errorlevel% "Could not make a directory %1"
+popd
+goto:eof
+
+:createNuget
+pushd %REPOSITORY_DIR%
+call bin\createNuget.bat -b -t -p -s c:\nugetPackages
+if ERRORLEVEL 1 call:failure %errorlevel% "Could not make a directory %1"
+popd
+goto:eof
+:failure
+echo.
+
+echo ERROR: %~2
+
+echo.
+echo FAILURE: Could not create a nuget package.
+
+set failure=%~1
+
+goto:eof
+
+:done
+
+echo.
+echo Success: ORTC nuget package is created.
+echo.
+:end
