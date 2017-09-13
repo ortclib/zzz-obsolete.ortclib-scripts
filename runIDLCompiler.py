add19b9b8caa9fc18b5e0b4b1d0be8b6abfbc002
Sergej Jovanovic <sergej@gnedo.com>
2017-09-13 14:46:59 +0200

    Added python script that generates gni file with generated sources

--- runIDLCompiler.py
+++ runIDLCompiler.py
@@ -1,7 +1,6 @@
 import os
 import sys
 
-
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 inputArray=sys.argv
@@ -9,8 +8,22 @@ inputArray=sys.argv
 idlPath=inputArray[1]
 compilerPath="zslib-eventing-tool-compiler"
 
+currentWorkingPath=os.getcwd()
+
+print "runIDLCompiler - CurrentWorkingPath:" + currentWorkingPath
 
 compilerNewPath = os.getcwd() + "/" + compilerPath;
 os.chdir(os.path.dirname(idlPath))
 
-os.system(compilerNewPath + " -idl cx c dotnet json wrapper -c config.json -o .")
+print "runIDLCompiler - NewWorkingPath:" + os.getcwd()
+
+#os.system(compilerNewPath + " -idl cx c dotnet json wrapper -c config.json -o .")
+os.system(compilerNewPath + " -idl c json wrapper -c config.json -o .")
+
+updateScriptPath = os.path.dirname(os.path.realpath(__file__)) + "/../../../../bin/updateGniFileWithSources.py"
+
+print "runIDLCompiler - UpdateScriptPath:" + updateScriptPath
+
+os.system("python " + updateScriptPath + " wrapper ortc_sources")
+
+os.chdir(os.path.dirname(currentWorkingPath))
