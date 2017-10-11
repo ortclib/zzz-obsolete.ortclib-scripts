162f4bcae88b3f821054de136d21a16d3b374801
Sergej Jovanovic <sergej@gnedo.com>
2017-10-11 20:30:45 +0200

    Disabled ARM platform. Updated paths for M62

--- prepareWebRtc.bat
+++ prepareWebRtc.bat
@@ -121,7 +121,7 @@ SET validInput=0
 SET messageText=
 
 IF /I "%platform%"=="all" (
-	SET platform_ARM=1
+	SET platform_ARM=0
 	SET platform_x64=1
 	SET platform_x86=1
 	SET platform_win32=1
@@ -220,36 +220,33 @@ GOTO:EOF
 CALL:print %trace% "Executing makeJunctionLinks function"
 
 CALL:makeLink . buildtools ..\buildtools
-CALL:makeLink . build ..\chromium-pruned\build
-CALL:makeLink . chromium\src\third_party\jsoncpp ..\chromium-pruned\third_party\jsoncpp
+CALL:makeLink . build ..\chromium\build
+CALL:makeLink . chromium\src\third_party\jsoncpp ..\chromium\third_party\jsoncpp
 CALL:makeLink . chromium\src\third_party\jsoncpp\source ..\jsoncpp
-CALL:makeLink . chromium\src\tools\protoc_wrapper ..\chromium-pruned\tools\protoc_wrapper
-CALL:makeLink . chromium\src\third_party\protobuf ..\chromium-pruned\third_party\protobuf
-CALL:makeLink . chromium\src\third_party\yasm ..\chromium-pruned\third_party\yasm
-CALL:makeLink . chromium\src\third_party\opus ..\chromium-pruned\third_party\opus
-::CALL:makeLink . chromium\src\third_party\colorama ..\chromium-pruned\third_party\colorama
-CALL:makeLink . chromium\src\third_party\boringssl ..\chromium-pruned\third_party\boringssl
-CALL:makeLink . chromium\src\third_party\usrsctp ..\chromium-pruned\third_party\usrsctp
-CALL:makeLink . chromium\src\third_party\libvpx ..\chromium-pruned\third_party\libvpx
+::CALL:makeLink . chromium\src\tools\protoc_wrapper ..\chromium\tools\protoc_wrapper
+CALL:makeLink . chromium\src\third_party\protobuf ..\chromium\third_party\protobuf
+CALL:makeLink . chromium\src\third_party\yasm ..\chromium\third_party\yasm
+CALL:makeLink . chromium\src\third_party\opus ..\chromium\third_party\opus
+CALL:makeLink . chromium\src\third_party\boringssl ..\chromium\third_party\boringssl
+CALL:makeLink . chromium\src\third_party\usrsctp ..\chromium\third_party\usrsctp
+CALL:makeLink . chromium\src\third_party\libvpx ..\chromium\third_party\libvpx
 CALL:makeLink . chromium\src\third_party\libvpx\source\libvpx ..\libvpx
-CALL:makeLink . chromium\src\testing ..\chromium-pruned\testing
+CALL:makeLink . chromium\src\testing ..\chromium\testing
 CALL:makeLink . testing chromium\src\testing
-CALL:makeLink . tools\protoc_wrapper chromium\src\tools\protoc_wrapper
+::CALL:makeLink . tools\protoc_wrapper chromium\src\tools\protoc_wrapper
 CALL:makeLink . third_party\yasm chromium\src\third_party\yasm
 CALL:makeLink . third_party\yasm\binaries ..\yasm\binaries
 CALL:makeLink . third_party\yasm\source\patched-yasm ..\yasm\patched-yasm
 CALL:makeLink . third_party\opus chromium\src\third_party\opus
 CALL:makeLink . third_party\opus\src ..\opus
-::CALL:makeLink . third_party\colorama chromium\src\third_party\colorama
-::CALL:makeLink . third_party\colorama\src ..\webrtc-deps\colorama
 CALL:makeLink . third_party\boringssl chromium\src\third_party\boringssl
 CALL:makeLink . third_party\boringssl\src ..\boringssl
 CALL:makeLink . third_party\usrsctp chromium\src\third_party\usrsctp
 CALL:makeLink . third_party\usrsctp\usrsctplib ..\usrsctp
 CALL:makeLink . third_party\protobuf chromium\src\third_party\protobuf
-CALL:makeLink . chromium\src\third_party\expat ..\chromium-pruned\third_party\expat
+CALL:makeLink . chromium\src\third_party\expat ..\chromium\third_party\expat
 CALL:makeLink . third_party\expat chromium\src\third_party\expat
-CALL:makeLink . chromium\src\third_party\googletest ..\chromium-pruned\third_party\googletest
+CALL:makeLink . chromium\src\third_party\googletest ..\chromium\third_party\googletest
 CALL:makeLink . third_party\googletest chromium\src\third_party\googletest
 CALL:makeLink . third_party\googletest\src ..\googletest
 CALL:makeLink . third_party\libsrtp ..\libsrtp
@@ -264,9 +261,8 @@ CALL:makeLink . third_party\gflags ..\gflags-build
 CALL:makeLink . third_party\gflags\src ..\gflags
 CALL:makeLink . third_party\winsdk_samples ..\winsdk_samples_v71
 CALL:makeLink . tools\gyp ..\gyp
-CALL:makeLink . tools\clang ..\chromium-pruned\tools\clang
-::CALL:makeLink . testing\gtest ..\googletest
-::CALL:makeLink . testing\gmock ..\googlemock
+::CALL:makeLink . tools\clang ..\chromium\tools\clang
+
 
 GOTO:EOF
 
@@ -275,10 +271,10 @@ GOTO:EOF
 ::XCopy  /S /I /Y ..\gflags-build third_party\gflags > NUL
 ::IF !errorlevel! NEQ 0 CALL:error 1 "Missing gn files for gflags"
 
-COPY ..\chromium-pruned\third_party\BUILD.gn third_party\BUILD.gn 
-COPY ..\chromium-pruned\third_party\DEPS third_party\DEPS 
-COPY ..\chromium-pruned\third_party\OWNERS third_party\OWNERS 
-COPY ..\chromium-pruned\third_party\PRESUBMIT.py third_party\PRESUBMIT.py 
+COPY ..\chromium\third_party\BUILD.gn third_party\BUILD.gn 
+COPY ..\chromium\third_party\DEPS third_party\DEPS 
+COPY ..\chromium\third_party\OWNERS third_party\OWNERS 
+COPY ..\chromium\third_party\PRESUBMIT.py third_party\PRESUBMIT.py 
 GOTO:EOF
 
 :setupDepotTools
